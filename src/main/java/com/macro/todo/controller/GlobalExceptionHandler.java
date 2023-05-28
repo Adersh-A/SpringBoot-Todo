c338907231d3e89bf777adcbcc2d9ef505931dc0
Adersh A <adershajith1999@gmail.com>
2023-05-28 16:49:46 +0530

    added global exception handler

---
+++ src/main/java/com/macro/todo/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.macro.todo.controller;
+
+import com.macro.todo.domain.ErrorDetails;
+import com.macro.todo.exception.TodoNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TodoNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+}
